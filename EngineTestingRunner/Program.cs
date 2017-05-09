0534d8b8f7ab857b5cb4c3ef482ca6ee47eb8c44
Im Unscripted <bareissvincent@gmail.com>
2017-05-09 23:49:01 +0200

    Projektdateien hinzufügen.

---
+++ EngineTestingRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace EngineTesting
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+        }
+    }
+}
---
+++ EngineTestingRunner/src/CoreEngine.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace EngineTesting.src
+{
+    class CoreEngine
+    {
+
+    }
+}
